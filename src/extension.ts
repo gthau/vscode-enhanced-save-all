2fa143f23f63c789e50dc355f7439700deac6712
Ghislain Thau <gthau@dalet.com>
2022-05-12 19:01:35 +0200

    added "Save All In Group except Untitled"

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { Uri } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,6 +10,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+	registerSaveAllCommand(context);
+	registerSaveAllInGroupCommand(context);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
+
+function registerSaveAllCommand(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.save-all-except-untitled', () => {
 		vscode.workspace.saveAll(/* includeUntitled */ false).then((res) => {
 			if (!res) {
@@ -20,5 +29,25 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function registerSaveAllInGroupCommand(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.save-all-in-group-except-untitled', () => {
+		const allUrisInGroup = vscode.window.tabGroups.activeTabGroup.tabs
+		  .map((tab) => tab.input)
+			.filter(inputHasUri)
+			.map(({ uri }) => uri);
+
+		const textDocumentsToSave = vscode.workspace.textDocuments.filter(({ isDirty, isUntitled, uri }) => (
+			isDirty &&
+			!isUntitled &&
+			allUrisInGroup.some((uriInGroup) => uriInGroup.path === uri.path)
+		));
+
+		textDocumentsToSave.forEach((document) => document.save());
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function inputHasUri<T>(input: T): input is T & { uri: Uri } {
+	return (input as any)?.uri instanceof Uri;
+}
