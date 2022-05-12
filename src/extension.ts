29124cdb365ab940f3b0f2517514ac4e818225de
Ghislain Thau <gthau@dalet.com>
2022-05-12 20:04:42 +0200

    changed indentation to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -5,49 +5,49 @@ import { Uri } from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "vscode-enhanced-save-all" is now active!');
+  console.log('Congratulations, your extension "vscode-enhanced-save-all" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	registerSaveAllCommand(context);
-	registerSaveAllInGroupCommand(context);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  registerSaveAllCommand(context);
+  registerSaveAllInGroupCommand(context);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
 function registerSaveAllCommand(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.save-all-except-untitled', () => {
-		vscode.workspace.saveAll(/* includeUntitled */ false).then((res) => {
-			if (!res) {
-				vscode.window.showErrorMessage('Failed to Save All except Untitled');
-			}
-		});
-	});
+  let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.save-all-except-untitled', () => {
+    vscode.workspace.saveAll(/* includeUntitled */ false).then((res) => {
+      if (!res) {
+        vscode.window.showErrorMessage('Failed to Save All except Untitled');
+      }
+    });
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 function registerSaveAllInGroupCommand(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.save-all-in-group-except-untitled', () => {
-		const allUrisInGroup = vscode.window.tabGroups.activeTabGroup.tabs
-		  .map((tab) => tab.input)
-			.filter(inputHasUri)
-			.map(({ uri }) => uri);
+  let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.save-all-in-group-except-untitled', () => {
+    const allUrisInGroup = vscode.window.tabGroups.activeTabGroup.tabs
+      .map((tab) => tab.input)
+      .filter(inputHasUri)
+      .map(({ uri }) => uri);
 
-		const textDocumentsToSave = vscode.workspace.textDocuments.filter(({ isDirty, isUntitled, uri }) => (
-			isDirty &&
-			!isUntitled &&
-			allUrisInGroup.some((uriInGroup) => uriInGroup.path === uri.path)
-		));
+    const textDocumentsToSave = vscode.workspace.textDocuments.filter(({ isDirty, isUntitled, uri }) => (
+      isDirty &&
+      !isUntitled &&
+      allUrisInGroup.some((uriInGroup) => uriInGroup.path === uri.path)
+    ));
 
-		textDocumentsToSave.forEach((document) => document.save());
-	});
+    textDocumentsToSave.forEach((document) => document.save());
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 function inputHasUri<T>(input: T): input is T & { uri: Uri } {
-	return (input as any)?.uri instanceof Uri;
+  return (input as any)?.uri instanceof Uri;
 }
