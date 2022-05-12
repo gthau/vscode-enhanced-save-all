f63edaffa8b7c134c62eb4c2df29ddbd872d0349
Ghislain Thau <gthau@dalet.com>
2022-05-12 17:36:11 +0200

    implemented command save-all-except-untitled

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
 	console.log('Congratulations, your extension "vscode-enhanced-save-all" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Enhanced Save All!');
+	let disposable = vscode.commands.registerCommand('vscode-enhanced-save-all.save-all-except-untitled', () => {
+		vscode.workspace.saveAll(/* includeUntitled */ false).then((res) => {
+			if (!res) {
+				vscode.window.showErrorMessage('Failed to Save All except Untitled');
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
